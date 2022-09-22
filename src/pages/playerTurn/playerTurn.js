7d12cd422ef33401f8845a61d027ae50cf9516ee
josh dietrich <jbdietrich2@gmail.com>
2022-09-22 14:15:45 -0600

    completed basic player turn area styles

--- src/pages/playerTurn/playerTurn.js
+++ src/pages/playerTurn/playerTurn.js
@@ -1,9 +1,54 @@
+import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
-function PlayerTurnPage() {
+import playerTurnStyles from "./playerTurn.styles";
+
+function PlayerTurnPage({ navigation }) {
+  const [timer, setTimer] = useState(3);
+
+  const tickTimer = () => {
+    setTimeout(() => {
+      if (timer > 0) {
+        setTimer(timer - 1);
+      }
+      if (timer < 1) {
+        navigation.navigate("home");
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    tickTimer();
+  }, [timer]);
+
   return (
-    <View>
-      <Text>Player turn page</Text>
+    <View style={playerTurnStyles.playerTurnPageContainer}>
+      <View style={playerTurnStyles.timerContainer}>
+        <Text style={playerTurnStyles.timerText}>{timer + " Seconds"}</Text>
+      </View>
+      <View style={playerTurnStyles.middlePlayArea}>
+        <View style={playerTurnStyles.skipContainer}>
+          <Text style={playerTurnStyles.areaText}>Skip</Text>
+        </View>
+        <View style={playerTurnStyles.cardContainer}>
+          <View style={playerTurnStyles.cardImage}></View>
+          <View style={playerTurnStyles.cardTitle}>
+            <Text style={playerTurnStyles.cardTitleText}>Kevin James</Text>
+          </View>
+          <View style={playerTurnStyles.cardDescription}>
+            <Text style={playerTurnStyles.cardDescriptionText}>
+              star of paul blart mall cop, king of queens, and here come's the
+              boom
+            </Text>
+          </View>
+        </View>
+        <View style={playerTurnStyles.deckContainer}>
+          <Text style={playerTurnStyles.areaText}>Deck</Text>
+        </View>
+      </View>
+      <View style={playerTurnStyles.correctContainer}>
+        <Text style={playerTurnStyles.correctText}>Correct</Text>
+      </View>
     </View>
   );
 }
--- src/pages/playerTurn/playerTurn.styles.js
+++ src/pages/playerTurn/playerTurn.styles.js
@@ -1,5 +1,87 @@
 import { StyleSheet } from "react-native";
 
-const playerTurnStyles = StyleSheet.create({});
+const playerTurnStyles = StyleSheet.create({
+  playerTurnPageContainer: {
+    flex: 1,
+  },
+  timerContainer: {
+    flex: 0.5,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  timerText: {
+    marginBottom: 12,
+    fontSize: 32,
+  },
+  middlePlayArea: {
+    flex: 3,
+    flexDirection: "row",
+  },
+  skipContainer: {
+    flex: 0.5,
+    borderWidth: 2,
+    justifyContent: "center",
+    borderTopRightRadius: 1000000,
+    borderBottomRightRadius: 1000000,
+  },
+  cardContainer: {
+    flex: 3,
+    borderWidth: 2,
+    marginLeft: 12,
+    marginRight: 12,
+    borderRadius: 12,
+    alignItems: "center",
+  },
+  cardImage: {
+    flex: 3,
+    border: 2,
+    borderWidth: 2,
+    marginTop: 12,
+    width: "90%",
+    borderRadius: 12,
+  },
+  cardTitle: {
+    flex: 1,
+    width: "90%",
+    alignItems: "center",
+  },
+  cardTitleText: {
+    fontSize: 32,
+  },
+  cardDescription: {
+    flex: 3,
+    width: "90%",
+  },
+  cardDescriptionText: {
+    fontSize: 22,
+  },
+  deckContainer: {
+    flex: 0.5,
+    borderWidth: 2,
+    justifyContent: "center",
+    borderBottomLeftRadius: 10000000,
+    borderTopLeftRadius: 10000000,
+  },
+  correctContainer: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  correctText: {
+    borderWidth: 2,
+    padding: 12,
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+    fontSize: 32,
+    width: "70%",
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+  areaText: {
+    fontSize: 62,
+    textTransform: "uppercase",
+    alignSelf: "center",
+  },
+});
 
 export default playerTurnStyles;
